86d9c684cd102af237bb461983620db87825f8e6
giorgiov-ai <142822477+giorgiov-ai@users.noreply.github.com>
2023-12-23 15:13:58 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,38 +13,31 @@
 # limitations under the License.
 
 import streamlit as st
+import random
 from streamlit.logger import get_logger
+
 
 LOGGER = get_logger(__name__)
 
 
 def run():
+    while True:
     st.set_page_config(
-        page_title="Hello",
+        page_title="Temperature e pressioni",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Esempio dashboard con visuliazzazione T e P")
+    T = random.randint(10, 30)
+    P = random.radint(1,5)
+    temperature=st.container()
+    pressure=st.container()
+    temperature.write(T)
+    pressure.write(P)
+    
 
-    st.sidebar.success("Select a demo above.")
+    
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
 
 if __name__ == "__main__":
